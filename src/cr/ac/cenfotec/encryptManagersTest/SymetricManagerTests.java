2810581f2e1eec602f5e0a817f9e3445d7ac6ca5
lotusmz <mary_zap@hotmail.com>
2018-07-27 10:17:50 -0600

    test blowfish encription

--- src/cr/ac/cenfotec/encryptManagersTest/SymetricManagerTests.java
+++ src/cr/ac/cenfotec/encryptManagersTest/SymetricManagerTests.java
@@ -10,7 +10,8 @@ public class SymetricManagerTests {
 	private SymetricManager symetricManager = new SymetricManager();
 	private String keyName = "baseball";
 	private String messageName = "Test name";
-	private String message = "Asymetric encription test";
+	private String message = "AES-symetric encription test";
+	private String message2 = "Blowfish-symetric encription test";
 	
 	@Test
 	public void testAesEncription() throws Exception {
@@ -18,6 +19,14 @@ public class SymetricManagerTests {
 		symetricManager.encryptMessage(messageName, message, keyName, 2);
 		
 		assertEquals(message, symetricManager.decryptMessage(messageName, keyName, 2));
+	}
+	
+	@Test
+	public void testBlowfishEncription() throws Exception {
+		symetricManager.createKey(keyName);
+		symetricManager.encryptMessage(messageName, message2, keyName, 3);
+		
+		assertEquals(message2, symetricManager.decryptMessage(messageName, keyName, 3));
 	}
 
 }

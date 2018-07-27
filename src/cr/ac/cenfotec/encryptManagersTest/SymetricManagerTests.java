d94066a94ee02753518a1aa965e9bc81b10a658f
lotusmz <mary_zap@hotmail.com>
2018-07-27 10:11:35 -0600

    Inicio testing

--- src/cr/ac/cenfotec/encryptManagersTest/SymetricManagerTests.java
+++ src/cr/ac/cenfotec/encryptManagersTest/SymetricManagerTests.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class SymetricManagerTests {
+import cr.ac.cenfotec.encryptManagers.SymetricManager;
 
+public class SymetricManagerTests {
+	private SymetricManager symetricManager = new SymetricManager();
+	private String keyName = "baseball";
+	private String messageName = "Test name";
+	private String message = "Asymetric encription test";
+	
 	@Test
-	public void test() {
+	public void testAesEncription() {
 		fail("Not yet implemented");
 	}
 
